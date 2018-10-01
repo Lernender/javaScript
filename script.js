fc96704aaa4898dc52a7fbd110f356ac561e6740
Camille Peter <c.peter@woche-pass.ch>
2018-10-01 11:50:06 +0200

    Adapt to coding standards

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Spielfeld
-var field = document.getElementById("playing-field");
+var field = document.getElementById('playing-field');
 var fieldWidth = 0;
 var fieldHeight = 0;
 var startKey = 'Space';
-var gamestatus = false;
-var result = document.getElementById("scoring");
+var gameStatus = false;
+var score = document.getElementById('score');
 
 // Ball
 var ball = document.getElementById('ball');
@@ -19,53 +19,52 @@ var playerSpeed = 7;
 
 // Spieler 1
 var p1 = document.getElementById('p1');
-var p1top = 0;
-var p1left = 0;
-var p1hohe = 0;
-var p1Direktion = false;
-var p1keyUp = 'w';
-var p1keyDown = 's';
+var p1Top = 0;
+var p1Left = 0;
+var p1Height = 0;
+var p1Direction = false;
+var p1KeyUp = 'w';
+var p1KeyDown = 's';
 var p1Points = 0;
 
 // Spieler 2
 var p2 = document.getElementById('p2');
-var p2top = 0;
-var p2left = 0;
-var p2hohe = 0;
-var p2Direktion = false;
-// das ist die Tastatur für nach oben
-var p2keyUp = 'ArrowUp';
-var p2keyDown = 'ArrowDown';
+var p2Top = 0;
+var p2Left = 0;
+var p2Height = 0;
+var p2Direction = false;
+// Das sind die Pfeiltasten auf der Tastatur
+var p2KeyUp = 'ArrowUp';
+var p2KeyDown = 'ArrowDown';
 var p2Points = 0;
 
 // Spiel
 setSize();
-// falls die Grösse geändert wird, müssen die Grössen automatisch angepasst werden.
+// Falls die Grösse geändert wird, müssen die Grössen automatisch angepasst werden
 window.addEventListener('resize', setSize);
-// Event Key welcher die Funktion setKey aufruft
+// Event Key, welcher die Funktion setKey aufruft
 document.addEventListener('keydown', setKey);
 document.addEventListener('keyup', resetKey);
 
 
-// Beim drücken von der Leertaste wird die Funktion aufgerufen
+// Beim Drücken der Leertaste wird die Funktion aufgerufen
 function setKey(event) {
   if (event.code === startKey) {
     startGame();
   }
 
   switch (event.key) {
-    case p1keyUp:
-      p1Direktion = 'up';
+    case p1KeyUp:
+      p1Direction = 'up';
       break;
-    case p1keyDown:
-      p1Direktion = 'down';
+    case p1KeyDown:
+      p1Direction = 'down';
       break;
-    case p2keyUp:
-      p2Direktion = 'up';
+    case p2KeyUp:
+      p2Direction = 'up';
       break;
-    // Die Variablen werden hier genommen.
-    case p2keyDown:
-      p2Direktion = 'down';
+    case p2KeyDown:
+      p2Direction = 'down';
       break;
   }
 }
@@ -74,82 +73,84 @@ function startGame() {
   ballLeftSpeed = ballSpeed;
   ballTopSpeed = 1;
 
-  if (!gamestatus) {
-    gamestatus = true;
+  if (!gameStatus) {
+    gameStatus = true;
     window.requestAnimationFrame(animation);
   } else {
-    gamestatus = false;
+    gameStatus = false;
   }
 }
 
 function resetKey(event) {
-  if (event.key === p1keyUp || event.key === p1keyDown) {
-    p1Direktion = false;
+  if (event.key === p1KeyUp || event.key === p1KeyDown) {
+    p1Direction = false;
   }
 
-  if (event.key === p2keyUp || event.key === p2keyDown) {
-    p2Direktion = false;
+  if (event.key === p2KeyUp || event.key === p2KeyDown) {
+    p2Direction = false;
   }
 }
 
-// Die Variablen sind sind oben noch leer, sie werden aber mit dieser Funktion "gefüllt
+// Die Variablen sind oben noch leer, sie werden aber mit dieser Funktion gefüllt
 function setSize() {
   fieldWidth = field.offsetWidth;
   fieldHeight = field.offsetHeight;
 
-  p2top = p2.offsetTop;
-  p2left = p2.offsetLeft;
-  p2hohe = p2.offsetHeight;
-
-  p1top = p1.offsetTop;
-  p1left = p1.offsetLeft + p1.offsetWidth;
-  p1hohe = p1.offsetHeight;
-
   ballDiameter = ball.offsetWidth;
   ballLeft = ball.offsetLeft;
   ballTop = ball.offsetTop;
+
+  p1Top = p1.offsetTop;
+  p1Left = p1.offsetLeft + p1.offsetWidth;
+  p1Height = p1.offsetHeight;
+
+  p2Top = p2.offsetTop;
+  p2Left = p2.offsetLeft;
+  p2Height = p2.offsetHeight;
 }
 
 function animation() {
   setPlayer();
   setBall();
-  // Hier wird die position links vom Ball gesetzt.
+
+  // Hier wird die position links vom Ball gesetzt
   ball.style.left = ballLeft + 'px';
-  // Hier wird die position oben vom Ball gesetzt.
+  // Hier wird die position oben vom Ball gesetzt
   ball.style.top = ballTop + 'px';
 
-  p1.style.top = p1top + 'px';
-  p2.style.top = p2top + 'px';
+  p1.style.top = p1Top + 'px';
+  p2.style.top = p2Top + 'px';
 
-  // console.log(event);
   // Die Funktion ruft sich selber noch einmal auf, Ablauf wiederholt
-  if (gamestatus) {
+  if (gameStatus) {
     window.requestAnimationFrame(animation);
   }
 }
 
 function setPlayer() {
-  if (p1Direktion === 'up') {
-    p1top -= playerSpeed;
-  } else if (p1Direktion === 'down') {
-    p1top += playerSpeed;
+  if (p1Direction === 'up') {
+    p1Top -= playerSpeed;
+  } else if (p1Direction === 'down') {
+    p1Top += playerSpeed;
   }
 
-  if (p2Direktion === 'up') {
-    p2top -= playerSpeed;
-  } else if (p2Direktion === 'down') {
-    p2top += playerSpeed;
+  if (p2Direction === 'up') {
+    p2Top -= playerSpeed;
+  } else if (p2Direction === 'down') {
+    p2Top += playerSpeed;
   }
 }
 
 function getP1BallConnectPosition() {
-  var p1BallTopCenter = ballTop - ((p1hohe / 2) + p1top);
-  return (ballTopSpeedMax / (p1hohe / 2)) * p1BallTopCenter;
+  var p1BallTopCenter = ballTop - ((p1Height / 2) + p1Top);
+
+  return (ballTopSpeedMax / (p1Height / 2)) * p1BallTopCenter;
 }
 
 function getP2BallConnectPosition() {
-  var p2BallTopCenter = ballTop - ((p2hohe / 2) + p2top);
-  return (ballTopSpeedMax / (p2hohe / 2)) * p2BallTopCenter;
+  var p2BallTopCenter = ballTop - ((p2Height / 2) + p2Top);
+
+  return (ballTopSpeedMax / (p2Height / 2)) * p2BallTopCenter;
 }
 
 function setBall() {
@@ -157,11 +158,11 @@ function setBall() {
   ballTop = ball.offsetTop;
 
   // Berührt den Ball (p1 + Breite von P1) von links
-  if (p1left > ballLeft) {
+  if (p1Left > ballLeft) {
     // Ist der Ball innerhalb von oben im bereich von P1 der oberen Kante
-    if ((ballTop + (ballDiameter / 2)) > p1top) {
+    if ((ballTop + (ballDiameter / 2)) > p1Top) {
       // Ist der Ball von oben om bereich von P1 der unteren Kante
-      if ((ballTop + (ballDiameter / 2)) < (p1top + p1hohe)) {
+      if ((ballTop + (ballDiameter / 2)) < (p1Top + p1Height)) {
         // Ball Richtung umkehren (Links / Rechts)
         ballLeftSpeed = ballLeftSpeed * -1;
         ballTopSpeed += getP1BallConnectPosition();
@@ -170,11 +171,11 @@ function setBall() {
   }
 
   // Berührt den Ball p2 von links
-  if (p2left < (ballLeft + ballDiameter)) {
+  if (p2Left < (ballLeft + ballDiameter)) {
     // Ist der Ball innerhalb von oben im bereich von P2 der oberen Kante
-    if ((ballTop + (ballDiameter / 2)) > p2top) {
+    if ((ballTop + (ballDiameter / 2)) > p2Top) {
       // Ist der Ball von oben om bereich von P2 der unteren Kante
-      if ((ballTop + (ballDiameter / 2)) < (p2top + p2hohe)) {
+      if ((ballTop + (ballDiameter / 2)) < (p2Top + p2Height)) {
         // Ball umkehren, links - rechts
         ballLeftSpeed = ballLeftSpeed * -1;
         ballTopSpeed += getP2BallConnectPosition();
@@ -185,9 +186,9 @@ function setBall() {
   // Der Ball prallt an der linken Wand auf
   if (ballLeft < 0) {
     ballLeftSpeed = ballLeftSpeed * -1;
-    // Speiler 2 bekommt einen Punkt
+    // Spieler 2 bekommt einen Punkt
     p2Points += 1;
-    resuteSetText();
+    resetScore();
   }
 
   // Der Ball prallt an der rechten Wand auf
@@ -195,17 +196,18 @@ function setBall() {
     ballLeftSpeed = ballLeftSpeed * -1;
     // Spieler 1 bekommt einen Punkt
     p1Points += 1;
-    resuteSetText();
+    resetScore();
   }
 
 // Der Ball prallt oben und unten ab
   if ((ballTop + ballDiameter) > fieldHeight || ballTop < 0) {
     ballTopSpeed = ballTopSpeed * -1;
   }
+
   ballLeft += ballLeftSpeed;
   ballTop += ballTopSpeed;
 }
 
-function resuteSetText() {
- result.innerText = p1Points + ' : ' + p2Points;
+function resetScore() {
+  score.innerText = p1Points + ' : ' + p2Points;
 }
